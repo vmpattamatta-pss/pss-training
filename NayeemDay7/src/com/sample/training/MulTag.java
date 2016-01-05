cb10116ac487b0d40455fdd645523bcde9c6bb0c
Nayeem Syed <nayeemsyed029@gmail.com>
2016-01-05 17:26:16 +0530

    custom tags

---
+++ NayeemDay7/src/com/sample/training/MulTag.java
@@ -0,0 +1,36 @@
+package com.sample.training;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+public class MulTag extends SimpleTagSupport {
+
+	private String inpA;
+	private String inpB;
+
+	@Override
+	public void doTag() throws JspException, IOException {
+		super.doTag();
+		getJspContext().getOut().write("Multiplication of InpA :: " + inpA + " and InpB :: " + inpB + " is :: "
+				+ String.valueOf(Integer.parseInt(inpA) * Integer.parseInt(inpB)));
+	}
+
+	public String getInpA() {
+		return inpA;
+	}
+
+	public void setInpA(String inpA) {
+		this.inpA = inpA;
+	}
+
+	public String getInpB() {
+		return inpB;
+	}
+
+	public void setInpB(String inpB) {
+		this.inpB = inpB;
+	}
+
+}
